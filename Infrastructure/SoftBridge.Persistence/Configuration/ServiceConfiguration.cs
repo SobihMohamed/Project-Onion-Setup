b2ef842f516409dcb9e14c7a2263128cff8d83a9
khmo112233789 <khmo8192920@gmail.com>
2026-04-08 22:09:35 +0200

    Configuration Files Added

--- Infrastructure/SoftBridge.Persistence/Configuration/ServiceConfiguration.cs
+++ Infrastructure/SoftBridge.Persistence/Configuration/ServiceConfiguration.cs
@@ -5,7 +5,7 @@ using SoftBridge.Domain.Models.ServiceAggregates;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using IServiceProvider =  SoftBridge.Domain.Models.AccountAggregates.SProvider;
+//using IServiceProvider =  SoftBridge.Domain.Models.AccountAggregates.SProvider;
 
 namespace SoftBridge.Persistence.Configuration
 {
@@ -14,6 +14,9 @@ namespace SoftBridge.Persistence.Configuration
         public void Configure(EntityTypeBuilder<Service> builder)
         {
             builder.HasKey(x => x.Id);
+
+            builder.Property(i => i.Id)
+               .HasDefaultValueSql("NEWID()");
 
             builder.Property(x => x.Title)
                 .IsRequired()
@@ -25,12 +28,49 @@ namespace SoftBridge.Persistence.Configuration
 
             builder.HasQueryFilter(s => !s.IsDeleted);
 
+            builder.Property(s => s.Status)
+               .IsRequired()
+               .HasConversion<string>()
+               .HasMaxLength(30);
+
+            builder.HasIndex(x => x.Status)
+                .HasFilter("[IsDeleted] = 0");
+
+            builder.Property(s => s.CreatedAt)
+               .IsRequired()
+               .HasDefaultValueSql("GETUTCDATE()");
+
+            builder.Property(s => s.IsDeleted)
+               .IsRequired()
+               .HasDefaultValue(false);
+
             builder.HasOne(x => x.Provider)
                 .WithMany(x => x.Services)
                 .HasForeignKey(x => x.ProviderId)
                 .OnDelete(DeleteBehavior.Restrict);
 
+            builder.HasOne(x => x.Category)
+                .WithMany(x => x.Services)
+                .HasForeignKey(x => x.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
 
+            builder.HasMany(x => x.Images)
+               .WithOne(x => x.Service)
+               .HasForeignKey(x => x.ServiceId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(s => s.ServiceRequests)
+               .WithOne(r => r.Service)
+               .HasForeignKey(r => r.ServiceId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(s => s.Reviews)
+               .WithOne()
+               .HasForeignKey(r => r.ServiceId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+
+            builder.ToTable("Services");
 
 
         }
